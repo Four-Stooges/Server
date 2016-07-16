c898473742f9e2dc8e8ff7fcbd9c8d06d4aa5ec4
Aditya Agarwal <adityag2511@gmail.com>
2016-07-16 14:59:23 +0530

    Register function

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.post('/userLogin', function(req,res){
 		if(code == 0 ){
 			res.send('Valid');
 			//res.sendFile(__dirname + '/validuser.html');
+			//res.sendFile(__dirname + '/userhome.html');
 		}
 		else{
 			//res.sendFile(__dirname + '/invaliduser.html');
@@ -45,8 +46,8 @@ app.get('/userNGO', function(req,res){
 
 //Authenticat NGO credentials
 app.post('/userNGO', function(req,res){
-	testName =  req.body.uname,
-	testPwd = req.body.password
+	testName =  req.body.uname;
+	testPwd = req.body.password;
 
 	// Invoking the API to check for user login
 	var exec = require('child_process').exec('python3 ../APIs/auth_ngo.py ' +  testName + ' ' + testPwd,function(error,stdout,stderr){
@@ -55,6 +56,7 @@ app.post('/userNGO', function(req,res){
 		if(code == 0 ){
 			res.send('Valid');
 			//res.sendFile(__dirname + '/validngo.html');
+			//res.sendFile(__dirname + '/ngohome.html');
 		}
 		else{
 			//res.sendFile(__dirname + '/invalidngo.html');
@@ -63,5 +65,45 @@ app.post('/userNGO', function(req,res){
 	});
 });
 
+app.get('/registerUser', function(req,res){
+	res.sendFile(__dirname + '/public/resources/html/registerUser.html');
+});
+
+app.post('/registerUser', function(req,res){
+	 userName   = req.body.email;
+	 password   = req.body.pwd;
+	 firstName  = req.body.fname;
+	 lastName   = req.body.lname;
+	 phoneNo    = req.body.phone;
+	 parameters = userName+' '+password+' '+fname+' '+lname+' '+phone;
+	 var exec = require('child_process').exec('python3 ../APIs/user_db.py ' + parameters, function(error,stdout,stderr){});
+	 exec.on('exit',function(code){
+	 	if(code == 0){
+	 		res.sendFile(__dirname + '/public/resources/html/success.html');
+	 	}
+	 	else{
+	 		res.send('404 Error. Unable to upload!! ');
+	 	}
+	 });
+});
+
+app.get('/registerNGO', function(req,res){
+	res.sendFile(__dirname + '/public/resources/html/registerNGO.html');
+});
+
+app.post('/registerNGO', function(req,res){
+	 userName   = req.body.email;
+	 password   = req.body.pwd;
+	 parameters = userName+' '+password;
+	 var exec = require('child_process').exec('python3 ../APIs/ngo_db.py ' + parameters, function(error,stdout,stderr){});
+	 exec.on('exit',function(code){
+	 	if(code == 0){
+	 		res.sendFile(__dirname + '/public/resources/html/success.html');
+	 	}
+	 	else{
+	 		res.send('404 Error. Unable to upload!! ');
+	 	}
+	 });
+});
 app.listen(3000);
 console.log('Server running at http://127.0.0.1:3000/');
